10e9e2e3b5c6801b3154652207bcf77839c0cfc4
CaritoDasilva <caritodasilva@gmail.com>
2018-06-21 19:55:09 -0400

    intentando calcular porcentajes de lecturas

--- src/data.js
+++ src/data.js
@@ -18,12 +18,43 @@ window.computeUsersStats = (users, progress, courses) => {
   ).then((responseJsons) => { // Arreglo de respuestas en json
     responseJsons[0].forEach((estudiante) => {
       const users = Object.entries(responseJsons[0]);
-      const progreso = Object.entries(responseJsons[1]);
+      const progress = Object.entries(responseJsons[1]);
       const courses = Object.entries(responseJsons[2]);
-      const progresoEstudiante = progreso.find(elemento => elemento[0] === estudiante.id);
-      return container.innerHTML += `<p>${estudiante.name + JSON.stringify(progresoEstudiante)}</p>`;
+      const progresoEstudiante = progress.find(elemento => elemento[0] === estudiante.id);
+
+      console.log(progresoEstudiante);
+      //return container.innerHTML += `<p>${estudiante.name + JSON.stringify(progresoEstudiante)}</p>`;
+
+      let lectures = 0;
+      let lecturesCompleted = 0;
+      Object.entries(progresoEstudiante).forEach((progEst) => {
+        if (progEst.type === 'read') {
+          lectures++;
+        }
+        if (progEst.completed === 1) {
+          lecturesCompleted++;
+        }
+        let lecturesPercent = lecturesCompleted / lectures;
+      });
+
+
+      /*let lectures = Object.entries(progresoEstudiante).filter(x => x[1].type === 'read').length;
+      let lecturesCompleted = Object.entries(progresoEstudiante).filter(x => x[1].type === 'read' && x[1].completed === 1).length;
+      let lecturesPercent = lecturesCompleted / lectures; */
+
+
+
+      return container.innerHTML += `<p>${estudiante.name.toUpperCase() + lectures + lecturesCompleted}</p>`;
     });
   });
+
+  /*
+      return let stats = new Object();
+      stats.name = [];
+      stats.exercises = {};
+      stats.reads = {};
+      stats.quizzes = {};
+      */
 
   /*
   fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
