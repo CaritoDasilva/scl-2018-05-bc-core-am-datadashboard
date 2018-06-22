77e85ea21289d257812f71912e27988c4e443b46
CaritoDasilva <caritodasilva@gmail.com>
2018-06-22 13:19:57 -0400

    calcula unidades leidas

--- src/data.js
+++ src/data.js
@@ -20,22 +20,37 @@ window.computeUsersStats = (users, progress, courses) => {
       const users = Object.entries(responseJsons[0]);
       const progress = Object.entries(responseJsons[1]);
       const courses = Object.entries(responseJsons[2]);
-      const progresoEstudiante = progress.find(elemento => elemento[0] === estudiante.id);
+      const progresoEstudiante = progress.find(elemento => elemento[0] === estudiante.id)[1];
 
       console.log(progresoEstudiante);
       //return container.innerHTML += `<p>${estudiante.name + JSON.stringify(progresoEstudiante)}</p>`;
 
       let lectures = 0;
       let lecturesCompleted = 0;
-      Object.entries(progresoEstudiante).forEach((progEst) => {
-        if (progEst.type === 'read') {
-          lectures++;
+      let lecturesPercent = (lecturesCompleted / lectures) * 100;
+      let i;
+      for (i in progresoEstudiante) {
+        if (progresoEstudiante.hasOwnProperty(i)) {
+          let element = progresoEstudiante[i];
+          let unit;
+          for (unit of Object.values(element.units)) {
+            for (let part of Object.values(unit.parts)) {
+              if (part.type === 'read') {
+                lectures++;
+              }
+              if (part.completed === 1) {
+                lecturesCompleted++;
+              }
+            }
+          }
+          console.log(lectures);
         }
-        if (progEst.completed === 1) {
-          lecturesCompleted++;
-        }
-        let lecturesPercent = lecturesCompleted / lectures;
-      });
+      }
+
+      /*return readsInfo = new Object;
+      readsInfo.lectures = lectures,
+      readsInfo.lecturesCompleted = lecturesCompleted,
+      readsInfo.lecturesPercent = lecturesPercent,*/
 
 
       /*let lectures = Object.entries(progresoEstudiante).filter(x => x[1].type === 'read').length;
@@ -44,37 +59,37 @@ window.computeUsersStats = (users, progress, courses) => {
 
 
 
-      return container.innerHTML += `<p>${estudiante.name.toUpperCase() + lectures + lecturesCompleted}</p>`;
+      return container.innerHTML += `<p>${estudiante.name.toUpperCase() + ' ' + lectures + ' ' + lecturesCompleted}</p>`;
+
     });
-  });
 
-  /*
-      return let stats = new Object();
-      stats.name = [];
-      stats.exercises = {};
-      stats.reads = {};
-      stats.quizzes = {};
-      */
+    /*
+        return let stats = new Object();
+        stats.name = [];
+        stats.exercises = {};
+        stats.reads = {};
+        stats.quizzes = {};
+        */
 
-  /*
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Error en búsqueda');
-      }
-    }).then((usersJson) => {
-      console.log(usersJson);
-      Object.entries(usersJson).forEach((jsonElement) => {
-        Object.entries(jsonElement).forEach((name) => {
-          let myJSON = JSON.stringify(usersJson);
-          document.getElementById('infoStudent').innerHTML = myJSON;
+    /*
+    fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error en búsqueda');
+        }
+      }).then((usersJson) => {
+        console.log(usersJson);
+        Object.entries(usersJson).forEach((jsonElement) => {
+          Object.entries(jsonElement).forEach((name) => {
+            let myJSON = JSON.stringify(usersJson);
+            document.getElementById('infoStudent').innerHTML = myJSON;
+          });
         });
-      });
-    });*/
+      });*/
+  });
 };
-
 window.sortUsers = (users, orderB, orderDirection) => {
 
 };
